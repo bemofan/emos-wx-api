ff8834f7202af03027a1684ad223167cab722a9a
mofan <bemofan@163.com>
2021-02-18 05:13:02 +0800

    初始化配置

---
+++ src/main/java/com/mofan/emos/wx/EmosWxApiApplication.java
@@ -0,0 +1,17 @@
+package com.mofan.emos.wx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author mofan
+ * @date 2021/2/18 5:10
+ */
+@SpringBootApplication
+public class EmosWxApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmosWxApiApplication.class, args);
+    }
+
+}
