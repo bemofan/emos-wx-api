3360eea7f8031b4f7cb0e71711f47ab4a4a0bd9a
mofan <bemofan@163.com>
2021-02-19 00:22:21 +0800

    利用Swagger搭建 REST API

--- src/main/java/com/mofan/emos/wx/config/SwaggerConfig.java
+++ src/main/java/com/mofan/emos/wx/config/SwaggerConfig.java
@@ -31,38 +31,37 @@ public class SwaggerConfig {
     @Bean
     public Docket createRestApi() {
         Docket docket = new Docket(DocumentationType.SWAGGER_2);
-        // ApiInfoBuilder 用于在Swagger界面上添加各种信息
+
         ApiInfoBuilder builder = new ApiInfoBuilder();
         builder.title("EMOS在线办公系统");
-        ApiInfo apiInfo = builder.build();
-        docket.apiInfo(apiInfo);
-        // ApiSelectorBuilder 用来设置哪些类中的方法会生成到REST API中
+
+        ApiInfo info = builder.build();
+        docket.apiInfo(info);
+
         ApiSelectorBuilder selectorBuilder = docket.select();
-        //所有包下的类
         selectorBuilder.paths(PathSelectors.any());
-        //使用@ApiOperation的方法会被提取到REST API中
         selectorBuilder.apis(RequestHandlerSelectors.withMethodAnnotation(ApiOperation.class));
         docket = selectorBuilder.build();
-        /*
-         * 下面的语句是开启对JWT的支持，当用户用Swagger调用受JWT认证保护的方法，
-         * 必须要先提交参数（例如令牌）
-         */
-        //存储用户必须提交的参数
-        List<ApiKey> apikey = new ArrayList();
-        //规定用户需要输入什么参数
-        apikey.add(new ApiKey("token", "token", "header"));
-        docket.securitySchemes(apikey);
-        //如果用户JWT认证通过，则在Swagger中全局有效
+
+        ApiKey apiKey = new ApiKey("token", "token", "header");
+        List<ApiKey> apiKeyList = new ArrayList<>();
+        apiKeyList.add(apiKey);
+        docket.securitySchemes(apiKeyList);
+
         AuthorizationScope scope = new AuthorizationScope("global", "accessEverything");
-        AuthorizationScope[] scopeArray = {scope};
-        //存储令牌和作用域
-        SecurityReference reference = new SecurityReference("token", scopeArray);
-        List<SecurityReference> refList = new ArrayList();
-        refList.add(reference);
-        SecurityContext context = SecurityContext.builder().securityReferences(refList).build();
-        List<SecurityContext> cxtList = new ArrayList();
-        cxtList.add(context);
-        docket.securityContexts(cxtList);
+        AuthorizationScope[] scopes = {scope};
+
+        SecurityReference reference = new SecurityReference("token", scopes);
+        List<SecurityReference> referenceList = new ArrayList<>();
+        referenceList.add(reference);
+
+        SecurityContext context = SecurityContext.builder().securityReferences(referenceList).build();
+        List<SecurityContext> contextList = new ArrayList<>();
+        contextList.add(context);
+
+        docket.securityContexts(contextList);
+
         return docket;
+
     }
 }
